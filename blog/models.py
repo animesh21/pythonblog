2e833d804df1ac9b1b06032b8fd89b6bb8d93fd3
Animesh Kumar <animesh0721@gmail.com>
2017-02-27 19:31:57 +0530

    Adds test cases
    * adds test cases for Blog model

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,22 @@ from django.contrib.auth.models import User
 class BlogUser(User):
     pass
 
+
+class Blog(models.Model):
+    """
+    A blog created by user
+    """
+    user = models.ForeignKey('BlogUser')
+    title = models.CharField(max_length=512)
+    text = models.TextField()
+    timestamp = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('-timestamp', )
+
+    @classmethod
+    def get_top_10(cls):
+        pass
--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import Blog, BlogUser
 
 
+class BlogTest(TestCase):
+    """
+    tests different methods of Blog model
+    """
+    def setUp(self):
+        user = BlogUser.objects.create(username='animax', email='animesh0721@gmail.com', password='animax')
+        blog1 = Blog.objects.create(user=user, title='My first blog', text='Hello, World!')
+        blog2 = Blog.objects.create(user=user, title='My second blog', text='Hello, World!')
+        blog3 = Blog.objects.create(user=user, title='My third blog', text='Hello, World!')
+
+    def test_get_top_10(self):
+        blogs = Blog.get_top_10()
+        blogs_sorted = sorted(blogs, key=lambda x: x.timestamp, reverse=True)
+        self.assertEquals(blogs, blogs_sorted)
